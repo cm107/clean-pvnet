8618b4f7d6920981c58154f44d81508691bdb9b2
Clayton Mork <mork.clayton3@gmail.com>
2020-12-01 13:57:26 +0900

    Fixed some parameters in infer_all_weights.py

--- test/infer_all_weights.py
+++ test/infer_all_weights.py
@@ -2,7 +2,8 @@ from clean_pvnet.infer.pvnet_inferer import PVNetInferer
 from annotation_utils.linemod.objects import Linemod_Dataset, LinemodCamera
 from annotation_utils.coco.structs import COCO_Dataset
 from common_utils.path_utils import get_all_files_of_extension, get_rootname_from_path
-from common_utils.file_utils import make_dir_if_not_exists, delete_all_files_in_dir
+from common_utils.file_utils import make_dir_if_not_exists, delete_all_files_in_dir, file_exists
+from tqdm import tqdm
 
 img_dir = '/home/clayton/workspace/prj/data/misc_dataset/darwin_datasets/nihonbashi2/organized'
 linemod_dataset = Linemod_Dataset.load_from_path(f'{img_dir}/train.json')
@@ -21,11 +22,18 @@ dsize = (linemod_image_sample.width, linemod_image_sample.height)
 
 weights_dir = '/home/clayton/workspace/git/clean-pvnet/data/model/pvnet/custom'
 weight_path_list = get_all_files_of_extension(weights_dir, 'pth')
+weight_path_list.sort()
 infer_data_dump_dir = '/home/clayton/workspace/prj/data_keep/data/toyota/from_toyota/20201017/20201017_robot_camera/infer_dump'
 make_dir_if_not_exists(infer_data_dump_dir)
-delete_all_files_in_dir(infer_data_dump_dir, ask_permission=True)
+# delete_all_files_in_dir(infer_data_dump_dir, ask_permission=True)
+weights_pbar = tqdm(total=len(weight_path_list), unit='weight(s)')
 for weight_path in weight_path_list:
     rootname = get_rootname_from_path(weight_path)
+    weights_pbar.set_description(rootname)
+    pred_dump_path = f'{infer_data_dump_dir}/{rootname}.json'
+    if file_exists(pred_dump_path):
+        weights_pbar.update()
+        continue
     inferer = PVNetInferer(weight_path=weight_path)
     inferer.infer_coco_dataset(
         dataset=coco_dataset,
@@ -34,5 +42,6 @@ for weight_path in weight_path_list:
         K=K,
         blackout=True,
         dsize=dsize,
-        pred_dump_path=f'{infer_data_dump_dir}/{rootname}.json'
+        pred_dump_path=pred_dump_path
     )
+    weights_pbar.update()
