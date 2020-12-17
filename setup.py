b713ef5db03f9f1e8c6c671b520ffad79fed5f63
Clayton Mork <mork.clayton3@gmail.com>
2020-12-17 19:10:54 +0900

    Removed pyclay-common_utils as explicit dependency since it is installed by pyclay-annotation_utils

--- setup.py
+++ setup.py
@@ -47,8 +47,7 @@ setup(
         'tensorboard',
         'pandas',
         'pylint==2.4.4',
-        'pyclay-common_utils==0.2.4',
-        'pyclay-annotation_utils @ https://github.com/cm107/annotation_utils/archive/0.3.7post0.zip'
+        'pyclay-annotation_utils @ https://github.com/cm107/annotation_utils/archive/development.zip'
     ],
     python_requires='>=3.7'
 )
